eb8d03807ab93bc8416169490d9f39ad063bd20c
DebrajLogic <debrajdey9696@gmail.com>
2024-02-07 04:43:58 -0500

    added controller

--- src/app.js
+++ src/app.js
@@ -21,4 +21,9 @@ app.use(express.static("public"));
 app.use(cookieParser());
 /* Configurations */
 
+// routes
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
 export { app };
