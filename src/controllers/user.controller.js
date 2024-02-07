c6bdde46cda13adc2e4124609d16584ed7b5a02a
DebrajLogic <debrajdey9696@gmail.com>
2024-02-07 08:33:50 -0500

    completed user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,11 +13,11 @@ const registerUser = asyncHandler(async (req, res) => {
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
   ) {
-    throw new ApiError(400, "fullname is required");
+    throw new ApiError(400, "some required field empty");
   }
 
   // validate - if user exists
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
   if (existedUser) {
@@ -26,7 +26,17 @@ const registerUser = asyncHandler(async (req, res) => {
 
   // check for images, check for avatar
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
+
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
   }
@@ -45,7 +55,7 @@ const registerUser = asyncHandler(async (req, res) => {
     avatar: avatar.url,
     coverImage: coverImage?.url || "",
     password,
-    username: username.toLowercase(),
+    username: username.toLowerCase(),
   });
 
   // remove password refresh token from response from db
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,6 +15,7 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     console.log("File uploaded Successfully!!", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); // unlink file if upload has failed
